eeaf867fbbab14e313cfa249f3bbc6cead6902c7
guoyu <guoyu@DESKTOP-I8V0FAL.lan>
2020-07-17 20:42:50 +0800

    11

--- HelloGithub/src/hello/HelloGithub.java
+++ HelloGithub/src/hello/HelloGithub.java
@@ -1,7 +1,7 @@
 package hello;
 
-public class HelloGithub {
-	public static void main(String[] args) {
-		System.out.println("Hello Github");
+public class HelloGithub {   
+	public static void main(String[] args) { 
+		System.out.println("Hello Github1111111111111111");
 	}
 }
